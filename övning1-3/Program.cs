ed8ca0040c473072219467cc78818c9c388311a3
joakim lundell selander <joakim.lundellselander@elev.ga.ntig.se>
2024-11-11 12:26:39 +0100

    first commit

---
+++ övning1-3/Program.cs
@@ -0,0 +1,29 @@
+﻿// // if (6 >= 3){
+// // Console.WriteLine("Hello, World!");
+// // }
+
+
+// Console.WriteLine("skriv ditt användarnamn");
+// string användarnamn = Console.ReadLine();
+// Console.WriteLine("skriv också in ditt lösenord");
+// string lösenord = Console.ReadLine();
+
+
+
+// if (användarnamn == "kalleanka" && lösenord == "12345")
+// {
+//     Console.WriteLine("välkommen in ");
+// }
+// else
+// {
+//     Console.WriteLine("nejsan mejsan");
+
+// }
+// Console.ReadLine();
+int count = 0;
+while (count< 32)
+{
+    Console.WriteLine("hello world");
+    count++;
+}
+Console.ReadLine();
